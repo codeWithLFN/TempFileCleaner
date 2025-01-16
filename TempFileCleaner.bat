e4a00a83e7a81a500f78c1fbcaccbb3e9ba79397
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2025-01-16 21:59:13 +0200

    Update TempFileCleaner.bat

--- TempFileCleaner.bat
+++ TempFileCleaner.bat
@@ -6,10 +6,10 @@
 pause
 
 color 3
-Del /S /F /Q %temp% >nul 2>&1
-Del /S /F /Q %Windir%\Temp >nul 2>&1
-Del /S /F /Q C:\WINDOWS\Prefetch >nul 2>&1
+Del/S/F/Q %temp%
+Del/S/F/Q %Windir%\Temp
+Del/S/F/Q C:\WINDOWS\Prefetch
 @echo.
-@echo All Temporary Files Successfully Deleted!
+@echo All Temporary Files SucessFully Deleted!
 @echo.
 pause
